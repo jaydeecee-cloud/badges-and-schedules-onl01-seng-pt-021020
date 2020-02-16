3e4ed06e055d608230ab4cedad21be74b083f16f
Joseph Choi <jdc07@live.com>
2020-02-16 20:49:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,11 @@ def batch_badge_creator(attendees)
   array
 end
 
-
-
-def printer(attendees)
-   batch_badge_creator(attendees).each { |names| puts names }
-   assign_rooms(attendees).each { |room| puts room }
+def assign_rooms(attendees)
+   array = []
+   attendees.each_with_index do |name, index|
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
 end
+
